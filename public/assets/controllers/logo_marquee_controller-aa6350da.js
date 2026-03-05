07fb94f41f80c19de79188215524c68aefd45c37
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2026-03-05 16:38:15 +0600

    basic structure setup home page

---
+++ public/assets/controllers/logo_marquee_controller-aa6350da.js
@@ -0,0 +1,51 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["track", "group"]
+
+  connect() {
+    this.offset = 0
+    this.paused = false
+    this.animationFrame = null
+    this.speed = window.innerWidth < 640 ? 0.45 : 0.65
+
+    requestAnimationFrame(() => {
+      this.groupWidth = this.groupTarget.scrollWidth
+      this.start()
+    })
+  }
+
+  disconnect() {
+    if (this.animationFrame) {
+      cancelAnimationFrame(this.animationFrame)
+    }
+  }
+
+  pause() {
+    this.paused = true
+  }
+
+  resume() {
+    this.paused = false
+  }
+
+  start() {
+    if (!this.groupWidth) return
+
+    const tick = () => {
+      if (!this.paused) {
+        this.offset -= this.speed
+
+        if (Math.abs(this.offset) >= this.groupWidth) {
+          this.offset += this.groupWidth
+        }
+
+        this.trackTarget.style.transform = `translate3d(${this.offset}px, 0, 0)`
+      }
+
+      this.animationFrame = requestAnimationFrame(tick)
+    }
+
+    this.animationFrame = requestAnimationFrame(tick)
+  }
+}
